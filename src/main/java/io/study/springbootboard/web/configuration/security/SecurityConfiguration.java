351275a2b725e72752b49e339f490b5fe100e68e
YunNote <zzdd1558@gmail.com>
2023-02-21 22:31:49 +0900

    feature: webSecurityt 설정을 통한 ignore antmatcher 지정

--- src/main/java/io/study/springbootboard/web/configuration/security/SecurityConfiguration.java
+++ src/main/java/io/study/springbootboard/web/configuration/security/SecurityConfiguration.java
@@ -8,7 +8,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,7 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Slf4j
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfiguration {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
    private final JwtProvider jwtProvider;
    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
@@ -42,10 +44,17 @@ public class SecurityConfiguration {
               .anyRequest().authenticated()
               .and()
               .formLogin().disable()
-              .apply(new JwtSecurityConfig(jwtProvider))
-      ;
-
+              .apply(new JwtSecurityConfig(jwtProvider));
       return http.build();
+   }
+
+   @Override
+   public void configure(WebSecurity web){
+
+      web.ignoring()
+              .antMatchers(
+                      "/favicon.ico"
+              );
    }
 
    @Bean
