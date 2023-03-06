568abc9711f2701ed71fa9b5cb0c7e3b167867ae
YunNote <zzdd1558@gmail.com>
2023-03-07 01:00:02 +0900

    feature: UserEntity email unique 속성 추가

--- src/main/java/io/study/springbootboard/api/user/domain/entity/User.java
+++ src/main/java/io/study/springbootboard/api/user/domain/entity/User.java
@@ -24,6 +24,7 @@ public class User extends BaseEntity {
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
 
+   @Column(unique = true)
    private String email;
    private String password;
 
--- src/test/java/io/study/springbootboard/api/user/domain/validate/UserValidatorTest.java
+++ src/test/java/io/study/springbootboard/api/user/domain/validate/UserValidatorTest.java
@@ -1,12 +1,12 @@
 package io.study.springbootboard.api.user.domain.validate;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 import io.study.springbootboard.api.user.domain.entity.User;
 import io.study.springbootboard.web.exception.types.user.UserEmailValidationException;
 import io.study.springbootboard.web.exception.types.user.UserPasswordValidationException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 
 public class UserValidatorTest {
@@ -19,7 +19,16 @@ public class UserValidatorTest {
       User user = User.generatedBasicUser("zzdd1558@gmail.com", "1234");
 
       assertThatThrownBy(() -> userValidator.validate(user))
-              .isInstanceOf(UserPasswordValidationException.class);
+         .isInstanceOf(UserPasswordValidationException.class);
+   }
+
+   @Test
+   @DisplayName(value = "[User] 존재하는 이메일 Test")
+   public void userEmailIsExist() {
+      User user = User.generatedBasicUser("zzdd1558@gmail.com", "1234");
+
+      assertThatThrownBy(() -> userValidator.validate(user))
+         .isInstanceOf(UserPasswordValidationException.class);
    }
 
    @Test
